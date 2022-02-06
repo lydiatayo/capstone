a6c64e07d4c98fb9f5decc611ef175ee289404f1
lydiatayo <lydia.peterDSFE@tech4dev.com>
2022-02-06 16:00:52 +0100

    Updated

--- src/components/Donate.js
+++ src/components/Donate.js
@@ -11,12 +11,12 @@ const Donate = () => {
             <Navbar />
 
          <header className="py-5" style={{ backgroundColor: '#40A5E9' }}>
-                <div class="container px-5">
-                    <div class="row gx-5 justify-content-center">
-                        <div class="col-lg-10 col-xxl-8">
-                            <div class="text-center my-5">
-                            <h1 class="fw-bolder text-white">Donate</h1>
-                                <p class="lead fw-normal text-white-50 mb-0">EEvery woman and girl has the right to live a life free from violence. Your donation can help them break the cycle of violence,
+                <div className="container px-5">
+                    <div className="row gx-5 justify-content-center">
+                        <div className="col-lg-10 col-xxl-8">
+                            <div className="text-center my-5">
+                            <h1 className="fw-bolder text-white">Donate</h1>
+                                <p className="lead fw-normal text-white-50 mb-0">EEvery woman and girl has the right to live a life free from violence. Your donation can help them break the cycle of violence,
                                     assist survivors, and drive economic inclusion. You donation will help survivors of violence to receive confidential assistance
                                     and support to stay safe.</p>
                             </div>
@@ -25,138 +25,137 @@ const Donate = () => {
                 </div>
             </header>
 
-            <section class="py-5">
-                <div class="container px-5 my-5">
-                    <div class="row gx-5">
-                        <div class="col-xl-8">
-                        <h5 class="mb-3">Donation Information</h5>
-                                <form class="needs-validation" novalidate>
-                                    <div class="row g-3">
-                                        <div class="col-sm-6">
-                                            <label for="firstName" class="form-label">First name</label>
-                                            <input type="text" class="form-control" id="firstName" placeholder="" value="" required />
-                                            <div class="invalid-feedback">
+            <section className="py-5">
+                <div className="container px-5 my-5">
+                    <div className="row gx-5">
+                        <div className="col-xl-8">
+                        <h5 className="mb-3">Donation Information</h5>
+                                <form className="needs-validation" novalidate>
+                                    <div className="row g-3">
+                                        <div className="col-sm-6">
+                                            <label for="firstName" className="form-label">First name</label>
+                                            <input type="text" className="form-control" id="firstName" placeholder="" value="" required />
+                                            <div className="invalid-feedback">
                                                 Valid first name is required.
                                             </div>
                                         </div>
 
-                                        <div class="col-sm-6">
-                                            <label for="lastName" class="form-label">Last name</label>
-                                            <input type="text" class="form-control" id="lastName" placeholder="" value="" required />
-                                            <div class="invalid-feedback">
+                                        <div className="col-sm-6">
+                                            <label for="lastName" className="form-label">Last name</label>
+                                            <input type="text" className="form-control" id="lastName" placeholder="" value="" required />
+                                            <div className="invalid-feedback">
                                                 Valid last name is required.
                                             </div>
                                         </div>
 
-                                        <div class="col-12">
-                                            <label for="email" class="form-label">Email</label>
-                                            <input type="email" class="form-control" id="email" placeholder="you@example.com" />
-                                            <div class="invalid-feedback">
+                                        <div className="col-12">
+                                            <label for="email" className="form-label">Email</label>
+                                            <input type="email" className="form-control" id="email" placeholder="you@example.com" />
+                                            <div className="invalid-feedback">
                                                 Please enter a valid email address.
                                             </div>
                                         </div>
 
-                                        <div class="col-12">
-                                            <label for="address" class="form-label">Address</label>
-                                            <input type="text" class="form-control" id="address" placeholder="1234 Main St" required />
-                                            <div class="invalid-feedback">
+                                        <div className="col-12">
+                                            <label for="address" className="form-label">Address</label>
+                                            <input type="text" className="form-control" id="address" placeholder="1234 Main St" required />
+                                            <div className="invalid-feedback">
                                                 Please enter your shipping address.
                                             </div>
                                         </div>
 
-                                        <div class="col-12">
-                                            <label for="address2" class="form-label">Address 2 <span class="text-muted">(Optional)</span></label>
-                                            <input type="text" class="form-control" id="address2" placeholder="Apartment or suite" />
+                                        <div className="col-12">
+                                            <label for="address2" className="form-label">Address 2 <span className="text-muted">(Optional)</span></label>
+                                            <input type="text" className="form-control" id="address2" placeholder="Apartment or suite" />
                                         </div>
 
-                                        <div class="col-md-6">
-                                            <label for="country" class="form-label">Country</label>
-                                            <select class="form-select" id="country" required>
+                                        <div className="col-md-6">
+                                            <label for="country" className="form-label">Country</label>
+                                            <select className="form-select" id="country" required>
                                                 <option value="">Choose...</option>
                                                 <option>Nigeria</option>
                                             </select>
-                                            <div class="invalid-feedback">
+                                            <div className="invalid-feedback">
                                                 Please select a valid country.
                                             </div>
                                         </div>
 
-                                        <div class="col-md-6">
-                                            <label for="state" class="form-label">State</label>
-                                            <select class="form-select" id="state" required>
+                                        <div className="col-md-6">
+                                            <label for="state" className="form-label">State</label>
+                                            <select className="form-select" id="state" required>
                                                 <option value="">Choose...</option>
                                                 <option>Lagos</option>
                                             </select>
-                                            <div class="invalid-feedback">
+                                            <div className="invalid-feedback">
                                                 Please provide a valid state.
                                             </div>
                                         </div>
                                     </div>
 
-                                    <hr class="my-4" />
 
-                                    <h5 class="mb-3">Payment</h5>
+                                    <h5 className="mb-3">Payment</h5>
 
-                                    <div class="my-3">
-                                        <div class="form-check">
-                                            <input id="credit" name="paymentMethod" type="radio" class="form-check-input" checked required />
-                                            <label class="form-check-label" for="credit">Credit card</label>
+                                    <div className="my-3">
+                                        <div className="form-check">
+                                            <input id="credit" name="paymentMethod" type="radio" className="form-check-input" checked required />
+                                            <label className="form-check-label" for="credit">Credit card</label>
                                         </div>
-                                        <div class="form-check">
-                                            <input id="debit" name="paymentMethod" type="radio" class="form-check-input" required />
-                                            <label class="form-check-label" for="debit">Debit card</label>
+                                        <div className="form-check">
+                                            <input id="debit" name="paymentMethod" type="radio" className="form-check-input" required />
+                                            <label className="form-check-label" for="debit">Debit card</label>
                                         </div>
-                                        <div class="form-check">
-                                            <input id="paypal" name="paymentMethod" type="radio" class="form-check-input" required />
-                                            <label class="form-check-label" for="paypal">PayPal</label>
+                                        <div className="form-check">
+                                            <input id="paypal" name="paymentMethod" type="radio" className="form-check-input" required />
+                                            <label className="form-check-label" for="paypal">PayPal</label>
                                         </div>
                                     </div>
 
-                                    <div class="row gy-3 mb-5">
-                                        <div class="col-md-6">
-                                            <label for="cc-name" class="form-label">Name on card</label>
-                                            <input type="text" class="form-control" id="cc-name" placeholder="" required />
-                                            <small class="text-muted">Full name as displayed on card</small>
-                                            <div class="invalid-feedback">
+                                    <div className="row gy-3 mb-5">
+                                        <div className="col-md-6">
+                                            <label for="cc-name" className="form-label">Name on card</label>
+                                            <input type="text" className="form-control" id="cc-name" placeholder="" required />
+                                            <small className="text-muted">Full name as displayed on card</small>
+                                            <div className="invalid-feedback">
                                                 Name on card is required
                                             </div>
                                         </div>
 
-                                        <div class="col-md-6">
-                                            <label for="cc-number" class="form-label">Credit card number</label>
-                                            <input type="text" class="form-control" id="cc-number" placeholder="" required />
-                                            <div class="invalid-feedback">
+                                        <div className="col-md-6">
+                                            <label for="cc-number" className="form-label">Credit card number</label>
+                                            <input type="text" className="form-control" id="cc-number" placeholder="" required />
+                                            <div className="invalid-feedback">
                                                 Credit card number is required
                                             </div>
                                         </div>
 
-                                        <div class="col-md-6">
-                                            <label for="cc-expiration" class="form-label">Expiration</label>
-                                            <input type="text" class="form-control" id="cc-expiration" placeholder="" required />
-                                            <div class="invalid-feedback">
+                                        <div className="col-md-6">
+                                            <label for="cc-expiration" className="form-label">Expiration</label>
+                                            <input type="text" className="form-control" id="cc-expiration" placeholder="" required />
+                                            <div className="invalid-feedback">
                                                 Expiration date required
                                             </div>
                                         </div>
 
-                                        <div class="col-md-6">
-                                            <label for="cc-cvv" class="form-label">CVV</label>
-                                            <input type="text" class="form-control" id="cc-cvv" placeholder="" required />
-                                            <div class="invalid-feedback">
+                                        <div className="col-md-6">
+                                            <label for="cc-cvv" className="form-label">CVV</label>
+                                            <input type="text" className="form-control" id="cc-cvv" placeholder="" required />
+                                            <div className="invalid-feedback">
                                                 Security code required
                                             </div>
                                         </div>
 </div>
-                                    <button class="btn btn-success btn-lg" type="submit">Make Donation</button>
+                                    <button className="btn btn-success btn-lg" type="submit">Make Donation</button>
                                 </form>
                         </div>
-                        <div class="col-xl-4">
-                            <div class="card border-0 bg-light mt-xl-5">
-                                <div class="card-body p-4 py-lg-5">
-                                    <div class="d-flex align-items-center justify-content-center">
-                                        <div class>
-                                        <h5 class="mb-3">Bank Transfer</h5>
-                                            <p class="text-muted mb-4"> Account Name: Unbound Organization.</p>
-                                            <p class="text-muted mb-4"> Account Number: 0029876773</p>
-                                            <p class="text-muted mb-4"> Bank: GTB</p>
+                        <div className="col-xl-4">
+                            <div className="card border-0 bg-light mt-xl-5">
+                                <div className="card-body p-4 py-lg-5">
+                                    <div className="d-flex align-items-center justify-content-center">
+                                        <div className>
+                                        <h5 className="mb-3">Bank Transfer</h5>
+                                            <p className="text-muted mb-4"> Account Name: Unbound Organization.</p>
+                                            <p className="text-muted mb-4"> Account Number: 0029876773</p>
+                                            <p className="text-muted mb-4"> Bank: GTB</p>
                                         </div>
                                     </div>
                                 </div>
--- src/components/Location.js
+++ src/components/Location.js
@@ -10,45 +10,6 @@ function Location () {
         <div>
             <Navbar/>
 
-            <header className="px-4 py-5">
-                 <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
-    <div class="carousel-indicators">
-      <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-      <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
-    </div>
-    <div class="carousel-inner">
-      <div class="carousel-item active" style={{ backgroundImage: `url(${headerhero}`}}>
-        <div class="carousel-caption">
-        <div className="my-5 text-xl-start">
-                                <h1 className="display-5 fw-bolder text-white mb-2">Need emergency help from an abusive situation?</h1>
-                                <Link to="/quickhelp"> <button className="btn btn-primary btn-lg px-4 me-sm-3">Get Quick Help</button> </Link>
-                                <Link to="/therapy"> <button className="btn btn-primary btn-lg px-4">Get Therapy Help</button> </Link>
-                            </div>
-        </div>
-      </div>
-      <div class="carousel-item" style={{ backgroundImage: `url(${headerhero}`}}>
-        <div class="carousel-caption">
-        <div className="my-5 text-xl-start">
-                                <h1 className="display-5 fw-bolder text-white mb-2">Need emergency help from an abusive situation?</h1>
-                                <Link to="/quickhelp"> <button className="btn btn-primary btn-lg px-4 me-sm-3">Get Quick Help</button> </Link>
-                                <Link to="/therapy"> <button className="btn btn-primary btn-lg px-4">Get Therapy Help</button> </Link>
-                            </div>
-        </div>
-      </div>
-
-      
-    </div>
-    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
-      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-      <span class="visually-hidden">Previous</span>
-    </button>
-    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
-      <span class="carousel-control-next-icon" aria-hidden="true"></span>
-      <span class="visually-hidden">Next</span>
-    </button>
-  </div>
-</header>
-
 
             <section>
                 <div className="container">
